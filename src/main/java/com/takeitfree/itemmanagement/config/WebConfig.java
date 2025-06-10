0ca6b55b4114ff641314c8966e8814ebc33d06e0
JKR23 <kuntualajulio7@gmail.com>
2025-06-10 00:15:09 -0400

    Allow_Foreign_Request_From_Frontend

---
+++ src/main/java/com/takeitfree/itemmanagement/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.takeitfree.itemmanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//allow foreign request
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000") // ton frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
--- src/main/java/com/takeitfree/itemmanagement/dto/StatusIdDTO.java
+++ src/main/java/com/takeitfree/itemmanagement/dto/StatusIdDTO.java
@@ -10,16 +10,19 @@ import java.io.Serializable;
 @Builder
 public class StatusIdDTO implements Serializable {
     private Long id;
+    private String statusName;
 
     public static StatusIdDTO toIdDTO(StatusDTO statusDTO){
         return StatusIdDTO.builder()
                 .id(statusDTO.getId())
+                .statusName(statusDTO.getName())
                 .build();
     }
 
     public static StatusDTO toDTO(StatusIdDTO statusIdDTO){
         return StatusDTO.builder()
                 .id(statusIdDTO.getId())
+                .name(statusIdDTO.getStatusName())
                 .build();
     }
 }
